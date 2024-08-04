ff3fa3c1aade45753452d238ac1f0e03f8e87f2f
Marvelous Osondu <marvnonso554@gmail.com>
2024-08-04 14:41:44 +0100

    Update Project files

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ title = "RepoRadar" }) => {
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
-          <FaGithub className="inline pr-3 text-4xl" />
+          <FaGithub className="inline pr-3 text-3xl" />
           <Link to="/" className="text-lg font-bold align-middle">
             {title}
           </Link>
@@ -14,10 +14,13 @@ const Navbar = ({ title = "RepoRadar" }) => {
 
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
-            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn text-sm">
               Home
             </Link>
-            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+            <Link
+              to="/about"
+              className="btn btn-ghost btn-sm rounded-btn text-sm"
+            >
               About
             </Link>
           </div>
